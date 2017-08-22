77b5fa47e7de49513a596ca4e93f547431c52ae7
Todd Greenstein <todd.greenstein@TG1.local>
2017-08-21 20:42:17 -0700

    Updated Examples to use native json

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Couchbase all in one Kafka Environment
 Builds a ready to use microservice environment using docker-compose   
 - Couchbase Service   
 - Zookeeper  
-- Schema Registry  
 - Kafka Broker   
 - Kafka Connect   
 - Kafka Rest Client
@@ -20,12 +19,29 @@ docker-compose up --build -d
 curl "http://localhost:8082/topics"   
 - **To list connectors**   
 curl "http://localhost:8083/connectors"   
-- **To send create an AVRO document and send it to a new topic**   
-curl -X POST -H "Content-Type: application/vnd.kafka.avro.v2+json" \   
--H "Accept: application/vnd.kafka.v2+json" \   
---data '{"key_schema": "{\"name\":\"user_id\"  ,\"type\": \"int\"   }", "value_schema": "{\"type\": \"record\", \"name\": \"User\", \"fields\": [{\"name\": \"name\", \"type\": \"string\"}]}", "records": [{"key" : 1 , "value": {"name": "testUser"}}]}' \   
-"http://localhost:8082/topics/bar"  
+- **To create new document and send it to a new topic**   
+curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" \
+  --data ' {
+  "records": [
+    {"key": {"schema": {"type":"string"},
+     "payload": "kafka::bar"},
+     "value": {"bar": "some random value", "bar_json_array_data":[
+	{"bar":"some random json value in an array"}],
+	"an_array_of_digits":[1,2,3,4,5,6]}
+    }
+  ]
+}' http://localhost:8082/topics/a_topic_with_json -w "\n"
 - **Initialize a sink to Couchbase using the Couchbase Kafka Connector**   
-curl -X POST -H "Content-Type: application/json" \   
-     --data '{"name": "cb", "config": {"connector.class":"com.couchbase.connect.kafka.CouchbaseSinkConnector","tasks.max":"1", "topics":"bar","connection.cluster_address":"cbdb","connection.bucket":"receiver","connection.password":"passreceiver"}}' \   
-    "http://localhost:8083/connectors"
+curl -X POST -H "Content-Type: application/json" --data '
+  {"name": "cb",
+   "config": {
+     "connector.class":"com.couchbase.connect.kafka.CouchbaseSinkConnector",
+     "tasks.max":"1",
+     "topics":"a_topic_with_json",
+     "connection.cluster_address":"cbdb",
+     "connection.bucket":"receiver",
+     "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+     "key.converter.schemas.enable": true,
+     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+     "value.converter.schemas.enable": false
+}}' http://localhost:8083/connectors -w "\n"
--- curl-examples.sh
+++ curl-examples.sh
@@ -6,20 +6,48 @@
 ##################################################
 
 ## List Topics
-curl -X GET "http://localhost:8082/topics"
+curl -X GET "http://localhost:8082/topics" -w "\n"
 
-curl -X GET "http://localhost:8083/connectors/"
+curl -X GET "http://localhost:8083/connectors/" -w "\n"
 
-### AVRO Send a full document specifying the key
-curl -X POST -H "Content-Type: application/vnd.kafka.avro.v2+json" \
-      -H "Accept: application/vnd.kafka.v2+json" \
-      --data '{"key_schema": "{\"name\":\"user_id\"  ,\"type\": \"int\"   }", "value_schema": "{\"type\": \"record\", \"name\": \"User\", \"fields\": [{\"name\": \"name\", \"type\": \"string\"}]}", "records": [{"key" : 1 , "value": {"name": "testUser"}}]}' \
-      "http://localhost:8082/topics/bar"
+## Send a JSON document and specify the key
+curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" \
+  --data '
+{
+  "records": [
+    {
+      "key": {"schema": {"type":"string"}, "payload": "kafka::foo"},
+      "value": {"foo": "bar"}
+    }
+  ]
+}' http://localhost:8082/topics/a_topic_with_json -w "\n"
+
+## Send different "schema" json document and key schema
+curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" \
+  --data '
+{
+  "records": [
+    {
+      "key": {"schema": {"type":"string"}, "payload": "kafka::bar"},
+      "value": {"foo": "bar", "more_foo":[{"foo":"bar"}]}
+    }
+  ]
+}' http://localhost:8082/topics/a_topic_with_json -w "\n"
 
 ## Create Connector
-curl -X POST -H "Content-Type: application/json" \
-     --data '{"name": "cb", "config": {"connector.class":"com.couchbase.connect.kafka.CouchbaseSinkConnector","tasks.max":"1", "topics":"bar","connection.cluster_address":"cbdb","connection.bucket":"receiver"}}' \
-    "http://localhost:8083/connectors"
+curl -X POST -H "Content-Type: application/json" --data '
+  {"name": "cb",
+   "config": {
+     "connector.class":"com.couchbase.connect.kafka.CouchbaseSinkConnector",
+     "tasks.max":"1",
+     "topics":"a_topic_with_json",
+     "connection.cluster_address":"cbdb",
+     "connection.bucket":"receiver",
+     "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+     "key.converter.schemas.enable": true,
+     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+     "value.converter.schemas.enable": false
+}}' http://localhost:8083/connectors -w "\n"
 
 ## Delete Connector
 ## curl -X DELETE localhost:8083/connectors/
